6c440cccb28a73aa548181aa0a60d1a7ef0b5d22
5gi <5giwastooshort@gmail.com>
2022-06-11 11:53:19 -0400

    BugFix 1.1.1

--- src/main/java/com/simplelifesteal/events/HeartUseEvent.java
+++ src/main/java/com/simplelifesteal/events/HeartUseEvent.java
@@ -11,15 +11,16 @@ import static com.simplelifesteal.SimpleLifeSteal.heart;
 public class HeartUseEvent implements Listener {
     @EventHandler
     public void HeartUse(PlayerInteractEvent e) {
-        if (e.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() < 40) {
-            if ((e.getPlayer().getInventory().getItemInMainHand().isSimilar(heart)) && (!e.getPlayer().getInventory().getItemInMainHand().equals(null))) {
+
+        if ((e.getPlayer().getInventory().getItemInMainHand().isSimilar(heart)) && (!e.getPlayer().getInventory().getItemInMainHand().equals(null))) {
+            if (e.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() < 40) {
                 Player player = e.getPlayer();
                 player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() + 2);
                 player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
                 player.sendMessage("§aYou have gained a heart from using a §c§lCrafted Heart§r§a!");
+            } else {
+                e.getPlayer().sendMessage("§cYou already have the maximum health!");
             }
-        } else {
-            e.getPlayer().sendMessage("§cYou already have the maximum health!");
         }
     }
 }
