c24558f49c647dc72b5d52b8cbd1369ef08e78e6
5gi <5giwastooshort@gmail.com>
2022-06-25 00:36:46 -0400

    added signature

---
+++ src/main/java/com/simplelifesteal/made_by_the5gi_on_github.java
@@ -0,0 +1,9 @@
+package com.simplelifesteal;
+
+import static java.lang.System.out;
+
+public class made_by_the5gi_on_github {
+    public void made_by_the5gi_on_github() {
+        out.println("Made by the5gi: https://www.spigotmc.org/resources/simplelifesteal.102533/");
+    }
+}
